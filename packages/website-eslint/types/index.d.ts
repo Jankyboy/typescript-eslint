7c7bd033589c541cfdd760316a75d62b6e860608
Armano <armano2@users.noreply.github.com>
2023-03-17 09:04:02 +0100

    fix: correct types

--- packages/website-eslint/types/index.d.ts
+++ packages/website-eslint/types/index.d.ts
@@ -2,7 +2,7 @@ import type { TSESLint } from '@typescript-eslint/utils';
 
 import type { analyze } from '@typescript-eslint/scope-manager/dist/analyze';
 import type { astConverter } from '@typescript-eslint/typescript-estree/dist/ast-converter';
-import type * as esquery from 'esquery';
+import esquery from 'esquery';
 import type { configs, rules } from '@typescript-eslint/eslint-plugin';
 
 export interface EslintUtilsModule {
